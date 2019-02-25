60409090ea9d380f94f7e24e0bad1ea267f1391d
Devon Akers <devon.p.akers@vanderbilt.edu>
2019-02-24 23:47:57 -0500

    can build DFA states for mod 4, base 10

--- main.py
+++ main.py
@@ -170,9 +170,10 @@ def carmichael(n):
 class Period:
     def __init__(self, seq):
         self.seq = seq
-        self.length, self.val, self.occ_1 = self.__period(self.seq)
-        self.i_seg = seq[:self.occ_1]
-        self.r_seg = seq[self.occ_1:self.occ_1 + self.length]
+        self.length, self.val, self.r_seg_start = self.__period(self.seq)
+        self.i_seg = seq[:self.r_seg_start]
+        self.r_seg = seq[self.r_seg_start:self.r_seg_start + self.length]
+        self.i_plus_r_seg = self.i_seg + self.r_seg
 
     def __period(self, seq):
         found = {}
@@ -183,7 +184,8 @@ class Period:
         raise RuntimeError("No repeating sequence found.")
 
     def __str__(self, *args, **kwargs):
-        return "Seq: %s; Initial segment: %s; Repeating segment: %s; Value: %s; Length: %s" % (self.seq, self.i_seg, self.r_seg, self.val, self.length)
+        return "Seq: %s; Initial segment: %s; Repeating segment: %s; Value: %s; Length: %s" % (
+            self.seq, self.i_seg, self.r_seg, self.val, self.length)
 
 
 def test_seq(mod, base=10):
@@ -193,5 +195,88 @@ def test_seq(mod, base=10):
     return seq
 
 
+def from_base_10(n_b10, b):
+    """Takes a number n_b10 in base 10 and returns it in the given base b."""
+    radix = b - 1  # Radix is one less than b b/c b is the number of unique digits
+    pow = 0
+    b_digits = []  # Digits in base b representation of the number
+    rem = lambda m_b10, b, pow: n_b10 - (b ** pow)
+    r_i = rem(n_b10, b, pow)
+    while r_i >= 0:
+        b_digits += [r_i]
+        pow += 1
+        n_b10 /= 10
+        r_i = rem(n_b10, b, pow)
+    return b_digits
+
+
+def __test_from_base_10(n_b10, b):
+    print("%s (base 10) converted to base %s: %s" % (n_b10, b, from_base_10(n_b10, b)))
+
+
+class DFA:
+    def __init__(self, mod, base, seq):
+        self.m = mod
+        self.base = base
+        self.w = base - 1
+        self.period = Period(seq)
+
+
+class DFAState:
+    def __init__(self, i, j, b, m, period):
+        self.i = i
+        self.j = j
+        self.b = b
+        self.m = m
+        self.period = period
+        self.tran_func = {d: self.__calc_delta_of(d) for d in range(b)}
+
+    def delta_of(self, d):
+        return self.tran_func[d]
+
+    def __str__(self):
+        str_ = "["
+        for d in range(self.b - 1):
+            str_ += "(%s, %s), " % (self.delta_of(d))
+        str_ += "(%s, %s)]" % (self.delta_of(self.b - 1))
+        return str_
+
+    def __in_init_seg(self):
+        return self.i < len(self.period.i_seg)
+
+    def next_in_i_seg(self):
+        return self.i + 1
+
+    def next_in_r_seg(self):
+        return self.period.r_seg_start + ((self.j + 1 - self.period.r_seg_start) % len(self.period.r_seg))
+
+    def current_period_val(self, in_i_seg=False):
+        return self.period.i_plus_r_seg[self.i]
+
+    def next_classifier_val(self, digit):
+        return (self.j + (digit * self.current_period_val())) % self.m
+
+    def __calc_delta_of(self, d):
+        """ Returns the state transitioned to when b is read in the current state (i,j)."""
+        if ((self.j % len(self.period.r_seg)) > self.b):
+            raise RuntimeError("Repeating sequence was too long to calculate transition function: %s" % (
+                self.j % len(self.period.r_seg) > self.b))
+        if self.__in_init_seg():
+            return self.i + 1, self.next_classifier_val(digit=d)
+        return self.next_in_r_seg(), self.next_classifier_val(digit=d)
+
+
 if __name__ == "__main__":
-    print(Period(test_seq(22)))
+    mod = 4
+    seq = [(10 ** p) % mod for p in range(2 * (mod + 1))]
+
+    period = Period(seq)
+    print("Period for division modulo %s: %s" % (mod, period))
+    n_states = 0
+    for  i in range(3):
+        for j in range(10):
+            n_states += 1
+            print("DFA state with i = %s, j = %s, b = %s, mod = %s: %s" % (
+            i, j, 10, mod, DFAState(i=i, j=j, m=mod, b=10, period=period)))
+    print("Number of states: %s" % n_states)
+
